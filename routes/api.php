aff4a9f59c5f9f87b9509666c479fb3b0ec39a3b
Konstantin <69kemolo69@gmail.com>
2023-08-07 21:03:21 +0300

    added resource ProductsController
    Signed-off-by: Konstantin <69kemolo69@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ProductsController;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::get("/products",[ProductController::class,"getProducts"]);
-Route::get("/products/{product_id}",[ProductController::class,"getProduct"]);
+//Route::get("/products",[ProductController::class,"getProducts"]);
+//Route::get("/products/{product_id}",[ProductController::class,"getProduct"]);
+//
+//Route::post("/products",[ProductController::class,"addProduct"]);
+//
+//Route::put("/products/{product_id}",[ProductController::class,"changeProducts"]);
+//
+//Route::patch("/products/{product_id}",[ProductController::class,"changeProduct"]);
+//
+//Route::delete("/products/{product_id}",[ProductController::class,"deleteProduct"]);
 
-Route::post("/products",[ProductController::class,"addProduct"]);
-
-Route::put("/products/{product_id}",[ProductController::class,"changeProducts"]);
-
-Route::patch("/products/{product_id}",[ProductController::class,"changeProduct"]);
-
-Route::delete("/products/{product_id}",[ProductController::class,"deleteProduct"]);
-
+Route::resource('products',ProductsController::class);
